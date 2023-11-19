3b2d1ab11bbceafa612c690d1994018c679d62f6
Vitaliy S <sofronov.vit@gmail.com>
2023-11-19 23:10:40 +0400

    initial project setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { providerAnimation } from "@angular/platform-browser/animations";
+import { TuiRootModule } from "@taiga-ui/core";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [providerAnimation(), provideRouter(routes), importProvidersFrom(TuiRootModule)]
 };
